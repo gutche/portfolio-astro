37006e94162c5d399a8d47c4d8be30bfc5099809
gutche <gledrianregalagutierrez@gmail.com>
2025-02-15 23:03:39 +0100

    feat: add CodeBlock component

---
+++ src/React/CodeBlock.tsx
@@ -0,0 +1,31 @@
+const CodeBlock = () => {
+	return (
+		<pre className="text-[var(--white)] text-sm md:text-md lg:text-xl font-medium leading-none font-[var(--font)] p-4 rounded-lg bg-[#1e1e1e] w-fit">
+			<code>
+				<span className="text-orange-400">const</span> name:{" "}
+				<span className="text-blue-500">string</span> ={" "}
+				<span className="text-green-500">"Gledrian Gutierrez"</span>;
+				<br />
+				<br />
+				<span className="text-orange-400">const</span> traits:{" "}
+				<span className="text-blue-500">
+					Traits <span className="text-yellow-400">[]</span>
+				</span>{" "}
+				= <span className="text-yellow-400">[</span>
+				<br />
+				&nbsp;&nbsp;
+				<span className="text-green-500">"Software Engineer"</span>,
+				<br />
+				&nbsp;&nbsp;
+				<span className="text-green-500">"Web Developer"</span>,
+				<br />
+				&nbsp;&nbsp;
+				<span className="text-green-500">"AI Enthusiast"</span>,
+				<br />
+				<span className="text-yellow-400">]</span>;
+			</code>
+		</pre>
+	);
+};
+
+export default CodeBlock;
