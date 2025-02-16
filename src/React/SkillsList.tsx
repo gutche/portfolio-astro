262fb5498a60c1a2e6d2bd3cd7c0afa9889939d3
gutche <gledrianregalagutierrez@gmail.com>
2025-02-16 01:33:19 +0100

    feat: modify SkillsList to my liking

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -56,58 +56,16 @@ const SkillsList = () => {
 
 	return (
 		<div className="text-left pt-3 md:pt-9">
-			<h3 className="text-[var(--white)] text-3xl md:text-4xl font-semibold md:mb-6">
-				What I do?
-			</h3>
-			<ul className="space-y-4 mt-4 text-lg">
-				{Object.entries(skills).map(([category, items]) => (
-					<li key={category} className="w-full">
-						<div
-							onClick={() => toggleItem(category)}
-							className="md:w-[400px] w-full bg-[#1414149c] rounded-2xl text-left hover:bg-opacity-80 transition-all border border-[var(--white-icon-tr)] cursor-pointer overflow-hidden">
-							<div className="flex items-center gap-3 p-4">
-								{CategoryIcons[category]}
-								<div className="flex items-center gap-2 flex-grow justify-between">
-									<div className="min-w-0 max-w-[200px] md:max-w-none overflow-hidden">
-										<span className="block truncate text-[var(--white)] text-lg">
-											{category}
-										</span>
-									</div>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 24 24"
-										fill="currentColor"
-										className={`w-6 h-6 text-[var(--white)] transform transition-transform flex-shrink-0 ${
-											openItem === category
-												? "rotate-180"
-												: ""
-										}`}>
-										<path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path>
-									</svg>
-								</div>
-							</div>
-
-							<div
-								className={`transition-all duration-300 px-4 ${
-									openItem === category
-										? "max-h-[500px] pb-4 opacity-100"
-										: "max-h-0 opacity-0"
-								}`}>
-								<ul className="space-y-2 text-[var(--white-icon)] text-sm">
-									{items.map((item, index) => (
-										<div
-											key={index}
-											className="flex items-center">
-											<span className="pl-1">•</span>
-											<li className="pl-3">{item}</li>
-										</div>
-									))}
-								</ul>
-							</div>
-						</div>
-					</li>
-				))}
-			</ul>
+			<div className="mockup-browser bg-neutral-800 border border-[var(--sec)]">
+				<div className="mockup-browser-toolbar">
+					<div className="input font-bold">What I do?</div>
+				</div>
+				<div className="bg-neutral-900 flex justify-center px-4 py-16 font-bold">
+					I’m primarily a Fullstack Web Developer, but I’m also
+					learning Mobile Development and AI, and I plan to get more
+					involved in both.
+				</div>
+			</div>
 		</div>
 	);
 };
