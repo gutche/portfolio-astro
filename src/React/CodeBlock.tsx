ed1b5bda8eb5abe695091ce7e13e73191d27815f
gutche <gledrianregalagutierrez@gmail.com>
2025-02-16 13:53:34 +0100

    styles: make CodeBlock look more like vim

--- src/React/CodeBlock.tsx
+++ src/React/CodeBlock.tsx
@@ -1,30 +1,41 @@
 const CodeBlock = () => {
 	return (
-		<pre className="text-[var(--white)] text-sm md:text-md lg:text-xl leading-none font-[var(--font)] p-4 rounded-lg bg-neutral-900 w-fit">
-			<code>
-				<span className="text-orange-400">const</span> name:{" "}
-				<span className="text-blue-500">string</span> ={" "}
-				<span className="text-green-500">"Gledrian Gutierrez"</span>;
-				<br />
-				<br />
-				<span className="text-orange-400">const</span> traits:{" "}
-				<span className="text-blue-500">
-					Traits <span className="text-yellow-400">[]</span>
-				</span>{" "}
-				= <span className="text-yellow-400">[</span>
-				<br />
-				&nbsp;&nbsp;
-				<span className="text-green-500">"Software Engineer"</span>,
-				<br />
-				&nbsp;&nbsp;
-				<span className="text-green-500">"Web Developer"</span>,
-				<br />
-				&nbsp;&nbsp;
-				<span className="text-green-500">"AI Enthusiast"</span>,
-				<br />
-				<span className="text-yellow-400">]</span>;
-			</code>
-		</pre>
+		<div>
+			<div className="bg-neutral-700 text-white px-4 py-1 rounded-t-md flex items-center justify-center relative">
+				<span className="absolute left-4 text-gray-400 text-lg">
+					● ● ●
+				</span>
+				<span className="text-white">AboutMe.ts</span>
+			</div>
+
+			<pre className="text-[var(--white)] text-sm md:text-md lg:text-lg font-[var(--font)] p-4 bg-neutral-900">
+				<code>
+					<span className="text-orange-400">const</span> name:{" "}
+					<span className="text-blue-500">string</span> ={" "}
+					<span className="text-green-500">"Gledrian Gutierrez"</span>
+					;
+					<br />
+					<br />
+					<span className="text-orange-400">const</span> traits:{" "}
+					<span className="text-blue-500">
+						Traits <span className="text-yellow-400">[]</span>
+					</span>{" "}
+					= <span className="text-yellow-400">[</span>
+					<br />
+					&nbsp;&nbsp;
+					<span className="text-green-500">"Software Engineer"</span>
+					,
+					<br />
+					&nbsp;&nbsp;
+					<span className="text-green-500">
+						"Fullstack Web Developer"
+					</span>
+					,
+					<br />
+					<span className="text-yellow-400">]</span>;
+				</code>
+			</pre>
+		</div>
 	);
 };
 
