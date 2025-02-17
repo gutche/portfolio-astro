0f7cb6d6533302e0f3c9196205b2f7b663a5b6fd
gutche <gledrianregalagutierrez@gmail.com>
2025-02-17 17:37:13 +0100

    fix: configure astro not to use image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,29 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  vite: {
-    resolve: {
-      alias: {
-        '@': '/src',
-        '@components': '/src/components'
-      }
-    }
-  },
-  output: 'static',
-  build: {
-    inlineStylesheets: 'auto'
-  },
-  server: {
-    host: true,
-    port: 4321,
-  }
+	integrations: [tailwind(), react()],
+	vite: {
+		resolve: {
+			alias: {
+				"@": "/src",
+				"@components": "/src/components",
+			},
+		},
+	},
+	output: "static",
+	build: {
+		inlineStylesheets: "auto",
+	},
+	server: {
+		host: true,
+		port: 4321,
+	},
+	image: {
+		service: passthroughImageService(),
+	},
 });
